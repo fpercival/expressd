0fbfbcd6a6f412258ce5464837faed7adea6b4c3
Frank <codeartist@frank-percival.net>
2017-05-10 10:08:33 +0200

    Moved log filter configuration

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 let config = require('json_config');
-let log = require('nodeutils').log;
 
 let APP = require('./index');
 
 config.init('.');
-
-log.setFilter(config.log.filter||'*');
-log.setLevel(config.log.level || 'info');
 
 let app = new APP(config);
 
